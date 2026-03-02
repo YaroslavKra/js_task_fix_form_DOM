5ba33bd660af83758b97a402d9189281b8448914
Yaroslav Kravchuk <Yaroslav.kravchyk@gmail.com>
2026-03-02 23:00:59 +0200

    Solution 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,26 +2,17 @@
 
 const form = document.querySelector('form');
 const inputs = form.querySelectorAll('input');
-
-// write code here
 inputs.forEach((input) => {
-  input.addEventListener('input', () => {
-    if (input.value.trim() === '') {
-      input.classList.add('error');
-    } else {
-      input.classList.remove('error');
-    }
-
     const label = document.createElement('label');
-
     label.classList.add('field-label');
     label.setAttribute('for', input.id);
     label.textContent = input.name;
+    input.parentElement.insertBefore(label, input);
 
     const placeholderText =
       input.name.charAt(0).toUpperCase() + input.name.slice(1);
 
     input.setAttribute('placeholder', placeholderText);
     input.parentElement.insertBefore(label, input);
-  });
+
 });
